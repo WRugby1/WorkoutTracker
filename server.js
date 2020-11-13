123fa4dc9f18decf0983426a6952c5f101a5e0ed
Tanner Austin <tanner.a.austin@gmail.com>
2020-11-13 09:52:51 -0600

    Heroku deployment

--- server.js
+++ server.js
@@ -23,71 +23,71 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-//API routes
-// app.put("/api/workouts/:id", (req, res) => {
-//     console.log(req.params.id)
-//     var body = req.body;
-//     db.Workout.findByIdAndUpdate(
-//         {
-//             _id: req.params.id
-//         },
-//         {
-//             $set: {
-//                 exercises: [{
-//                     type: body.type,
-//                     name: body.name,
-//                     distance: body.distance,
-//                     weight: body.weight,
-//                     sets: body.sets,
-//                     reps: body.reps,
-//                     duration: body.duration
-//                 }]
-//             }
-//         }
-//     ).then(data => {
-//         console.log(data);
-//         res.json(data);
-//     });
-// });
+// API routes
+app.put("/api/workouts/:id", (req, res) => {
+    console.log(req.params.id)
+    var body = req.body;
+    db.Workout.findByIdAndUpdate(
+        {
+            _id: req.params.id
+        },
+        {
+            $set: {
+                exercises: [{
+                    type: body.type,
+                    name: body.name,
+                    distance: body.distance,
+                    weight: body.weight,
+                    sets: body.sets,
+                    reps: body.reps,
+                    duration: body.duration
+                }]
+            }
+        }
+    ).then(data => {
+        console.log(data);
+        res.json(data);
+    });
+});
 
-// app.post("/api/workouts", ({ body }, res) => {
-//     db.Workout.create({
-//         exercises: [{
-//             type: body.type,
-//             name: body.name,
-//             totalDuration: body.totalDuration,
-//             weight: body.weight,
-//             reps: body.reps,
-//             sets: body.sets,
-//             distance: body.distance
-//         }]
-//     })
-//         .then(dbWorkout => {
-//             res.json(dbWorkout);
-//         })
-// });
+app.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create({
+        exercises: [{
+            type: body.type,
+            name: body.name,
+            totalDuration: body.totalDuration,
+            weight: body.weight,
+            reps: body.reps,
+            sets: body.sets,
+            distance: body.distance
+        }]
+    })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+});
 
-// app.get("/api/workouts", (req, res) => {
-//     db.Workout.find({}, (err, data) => {
-//         if (err) {
-//             console.log(err);
-//         }
-//         else {
-//             res.json(data);
-//         }
-//     })
-// });
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({}, (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.json(data);
+        }
+    })
+});
 
-// app.get("/api/workouts/range", (req, res) => {
-//     db.Workout.find({}, (err, data) => {
-//         if (err) {
-//             console.log(err);
-//         }
-//         else {
-//             res.json(data);
-//         }
-//     })
-// });
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}, (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.json(data);
+        }
+    })
+});
 
 // //HTML routes
 app.get("/exercise", (req, res) => {
