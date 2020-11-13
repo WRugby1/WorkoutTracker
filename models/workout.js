bf97e0a0f462ededfa446e362d96935fe4c8f1e8
Tanner Austin <tanner.a.austin@gmail.com>
2020-11-12 18:25:57 -0600

    MVP

--- models/workout.js
+++ models/workout.js
@@ -2,31 +2,36 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutsSchema = new Schema({
-  type: {
-      type: String,
-      required: "String is required"
-  },
-  name: {
-      type: String
-  },
-  distance: {
-      type: Number
-  },
-  weight: {
-      type: Number
-  },
-  sets: {
-      type: Number
-  },
-  reps: {
-      type: Number
-  },
-  duration: {
-      type: Number
-  },
-
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    exercises: [{
+        type: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String
+        },
+        distance: {
+            type: Number
+        },
+        weight: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        duration: {
+            type: Number
+        }
+    }]
 });
 
-const Workout = mongoose.model("Workouts", workoutsSchema);
+const Workout = mongoose.model("Workout", workoutsSchema);
 
 module.exports = Workout;
--- server.js
+++ server.js
@@ -4,39 +4,82 @@ const logger = require("morgan");
 const path = require("path");
 const app = express();
 const mongoose = require("mongoose");
+const db = require("./models/Workout");
 
-const databaseUrl = "FitnessTracker";
-const collections = ["Workouts"];
-
-const db = mongojs(databaseUrl, collections);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/FitnessTracker", {
+    useNewUrlParser: true
+});
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-db.on("error", error => {
-    console.log("Database Error:", error);
-});
-
-//example 9
-
 //API routes
-app.post("/api/workouts", (req, res) => {
-    body = req.body
-    db.Workout.save(body, (err, data) => {
-        if(err) {
-            console.log(err)
+app.put("/api/workouts/:id", (req, res) => {
+    console.log(req.params.id)
+    var body = req.body;
+    db.findByIdAndUpdate(
+        {
+            _id: req.params.id
+        },
+        {
+            $set: {
+                exercises: [{
+                    type: body.type,
+                    name: body.name,
+                    distance: body.distance,
+                    weight: body.weight,
+                    sets: body.sets,
+                    reps: body.reps,
+                    duration: body.duration
+                }]
+            }
         }
-        else {
-            res.send(data)
-        }
+    ).then(data => {
+        console.log(data);
+        res.json(data);
     });
 });
 
-app.put("/api/workouts/:id", (req, res) => {
+app.post("/api/workouts", ({ body }, res) => {
+    db.create({
+        exercise: {
+            type: body.type,
+            name: body.name,
+            totalDuration: body.totalDuration,
+            weight: body.weight,
+            reps: body.reps,
+            sets: body.sets,
+            distance: body.distance
+        }
+    })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+});
 
-})
+app.get("/api/workouts", (req, res) => {
+    db.find({}, (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.json(data);
+        }
+    })
+});
+
+app.get("/api/workouts/range", (req, res) => {
+    db.find({}, (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.json(data);
+        }
+    })
+});
 
 //HTML routes
 app.get("/exercise", (req, res) => {
